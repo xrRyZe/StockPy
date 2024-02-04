007d96f793605af0af6578f95a3aa5fcdd1322d2
Robin Kaiser <robin.n.kaiser@gmail.com>
2024-02-04 19:55:48 +0100

    Change: created function to fetch data of a stock and save it as a csv

--- stockpy/stock_file_handler/create_stock_data_file.py
+++ stockpy/stock_file_handler/create_stock_data_file.py
@@ -1,15 +1,30 @@
 import pandas as pd
 import yfinance as yf
+import datetime as dt
 
-# Symbol für das gewünschte Unternehmen (z. B. AAPL für Apple)
-ticker_symbol = 'AAPL'
-
-# Startdatum und Enddatum für die historischen Daten
 start_date = '2023-01-01'
-end_date = '2024-01-01'
+# end_date = '2024-01-01'
 
-# Abrufen der historischen Daten von Yahoo Finance
-data = yf.download(ticker_symbol, start=start_date, end=end_date)
+def create_stock_data_file(stock_short_code:str):
+    # Abrufen der historischen Daten von Yahoo Finance
+    data = yf.download(stock_short_code, start=start_date, end=dt.datetime.now().strftime('%Y-%m-%d'))
 
-# Speichern der Daten in eine CSV-Datei
-data.to_csv('./')
+    # Speichern der Daten in eine CSV-Datei
+    data.to_csv(f'./stock_data/{stock_short_code}.csv')
+    print(f"Daten von {stock_short_code} erfolgreich gespeichert.")
+
+create_stock_data_file('AAPL')
+
+
+
+# # Symbol für das gewünschte Unternehmen (z. B. AAPL für Apple)
+# ticker_symbol = 'AAPL'
+
+# # Startdatum und Enddatum für die historischen Daten
+
+# # Abrufen der historischen Daten von Yahoo Finance
+# data = yf.download(ticker_symbol, start=start_date, end=end_date)
+
+# # Speichern der Daten in eine CSV-Datei
+# data.to_csv('./stock_data/AAPL.csv')
+# print(f"Daten von {ticker_symbol} erfolgreich gespeichert.")
